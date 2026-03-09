2c5c2b689d956f13c6d9194807d5d6bf5692cbe5
Ibrahim Khalil Isma'il <ikhalilismail2020@gmail.com>
2026-03-09 12:44:05 +0100

    Add Khadija.cpp with personal information output

---
+++ Khadija.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Khadija Hassan Garba" << endl;
+    cout << "Khadijahassangarba05@email.com" << endl;
+    cout << "@khadijahassan" << endl;
+    cout << "Transcriptomics" << endl;
+    return 0;
+}
